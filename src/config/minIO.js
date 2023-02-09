43dc06ac7b7ea21295de92a4173c9a067a354258
Samuel Molendolff Teixeira <samuelmolendolff@hotmail.com>
2023-02-09 13:33:39 -0300

    chore: remove  version in vercel.json

--- src/config/minIO.js
+++ src/config/minIO.js
@@ -6,3 +6,12 @@ module.exports = {
     accessKey: process.env.MINIO_ACCESS_KEY,
     secretKey: process.env.MINIO_SECRET_KEY
 }
+
+// * for test local, please use this template: 
+/*
+    endPoint: process.env.MINIO_URL,
+    port: 9000,
+    useSSL: false,
+    accessKey: process.env.MINIO_ACCESS_KEY,
+    secretKey: process.env.MINIO_SECRET_KEY
+*/
