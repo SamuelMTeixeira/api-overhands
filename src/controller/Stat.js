52ae407025bd0c796f148df9b6ca52c0a50b7733
Samuel Molendolff Teixeira <samuelmolendolff@hotmail.com>
2023-02-23 01:11:50 -0300

    fix: foreach error not processing

--- src/controller/Stat.js
+++ src/controller/Stat.js
@@ -3,21 +3,23 @@ const Stat = require('../models/Stat')
 
 module.exports = {
     async store(req, res) {
-        // Receive all report activities
         const { reports } = req.body
 
         if (!reports) {
-            return await res.status(403).json({ error: 'Report is missing' })
+            return res.status(403).json({ error: 'Report is missing' })
         }
 
-        reports.forEach(async (report) => {
-            const stat = await Stat.create(report)
-
-            if (!stat) {
-                return res.status(400).json({ error: 'Error to create report' })
+        try {
+            for (const report of reports) {
+                const stat = await Stat.create(report)
+                if (!stat) {
+                    return res.status(400).json({ error: 'Error to create report' })
+                }
             }
-        })
 
-        return await res.json({ status: 'Reports successfully delivered' })
+            return res.json({ status: 'Reports successfully delivered' })
+        } catch (err) {
+            return res.status(500).json({ error: 'Internal server error' })
+        }
     }
 }
