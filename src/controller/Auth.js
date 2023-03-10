cd5841016884f17169fc5b4cdf8cde1403bade99
Samuel Molendolff Teixeira <samuelmolendolff@hotmail.com>
2023-03-10 09:45:26 -0300

    refactor: code optimization and query reduction

--- src/controller/Auth.js
+++ src/controller/Auth.js
@@ -6,10 +6,6 @@ const authConfig = require('../config/auth')
 const User = require('../models/User')
 const sequelize = require('../database')
 
-const generateToken = (id) => jwt.sign({ id }, authConfig.secret, {
-    expiresIn: "15d"
-})
-
 module.exports = {
     async login(req, res) {
         const { email, password } = req.body
@@ -19,80 +15,81 @@ module.exports = {
         })
 
         if (!user) {
-            return res.status(400).json({ error: 'Invalid email' })
+            return res.status(400).json({ error: 'User not found' })
         }
 
         if (!(await bcrypt.compare(password, user.password))) {
-            return res.status(400).json({ error: "Invalid password" });
+            return res.status(400).json({ error: "Invalid password" })
         }
 
-        const token = generateToken(user.id)
-
-        const queryXP = `
-            SELECT CASE WHEN SUM(a.xp) IS NULL THEN 0 ELSE FLOOR(SUM(a.xp)) END AS xp
-            FROM Users u
-            INNER JOIN Stats s ON s.Users_id = u.id
-            INNER JOIN Activities a ON a.id = s.Activity_id
-            WHERE u.id = ${user.id} AND s.isCorrect = true`
-
-        // Get XP user
-        const getXp = await sequelize.query(queryXP, { type: sequelize.QueryTypes.SELECT })
+        const xp = await getXp({
+            where: { email: user.email }
+        })
 
         return await res.json({
-            token,
+            token: generateToken(user.id),
             user: {
                 id: user.id,
                 name: user.name,
                 lastname: user.lastname,
                 email: user.email,
-                situation: user.situation,
                 birth: user.birth,
                 gender: user.gender,
-                xp: getXp[0].xp
+                xp
             }
         })
-
     },
 
     async register(req, res) {
         const { name, lastname, email, password, situation, gender, birth } = req.body
 
-        const isUserExists = await User.findOne({
-            where: { email }
-        })
-
-        if (isUserExists) {
-            return await res.status(400).json({ error: 'User already exists' })
-        }
-
         const hash = await bcrypt.hash(password, 10) // Criptografa a senha
 
-        const user = await User.create({ name, lastname, email, password: hash, gender, birth, situation, isAdmin: 0 })
+        const [user, created] = await User.findOrCreate({
+            where: { email },
+            defaults: { name, lastname, email, password: hash, gender, birth, situation, isAdmin: false },
+        })
 
-        const token = generateToken(user.id)
-
-        const queryXP = `
-            SELECT CASE WHEN SUM(a.xp) IS NULL THEN 0 ELSE FLOOR(SUM(a.xp)) END AS xp
-            FROM Users u
-            INNER JOIN Stats s ON s.Users_id = u.id
-            INNER JOIN Activities a ON a.id = s.Activity_id
-            WHERE u.id = ${user.id} AND s.isCorrect = true
-
-        `
-        // Get XP user
-        const getXp = await sequelize.query(queryXP, { type: sequelize.QueryTypes.SELECT })
+        if (!created) {
+            return res.status(400).json({ error: 'User already exists' })
+        }
 
         return await res.json({
-            token,
+            token: generateToken(user.id),
             user: {
                 id: user.id,
                 name: user.name,
                 lastname: user.lastname,
                 email: user.email,
-                gender: user.gender,
                 birth: user.birth,
-                xp: getXp[0].xp,
+                gender: user.gender,
+                xp: "0"
             }
         })
-    },
+
+    }
+
+}
+
+// Functions
+const generateToken = (id) => jwt.sign({ id }, authConfig.secret, {
+    expiresIn: "15d"
+})
+
+
+const getXp = async ({ where = { email: '' } }) => {
+
+    const { email } = where
+
+    const query = `
+    SELECT CASE WHEN COUNT(a.xp) = 0 THEN 0 ELSE SUM(a.xp) END AS xp
+    FROM Users u
+    INNER JOIN Stats s ON s.Users_id = u.id AND u.email LIKE '${email}'
+    INNER JOIN Activities a ON a.id = s.Activity_id
+    WHERE s.isCorrect = true
+    `
+    // Get XP user
+    const response = await sequelize.query(query, { type: sequelize.QueryTypes.SELECT })
+
+    return response[0].xp
 }
