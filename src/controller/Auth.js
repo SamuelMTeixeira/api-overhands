bee10f15ac45dfb7011d41e26aaa201781533852
Samuel Molendolff Teixeira <samuelmolendolff@hotmail.com>
2023-03-10 11:34:33 -0300

    refactor: Improvement of the login query

--- src/controller/Auth.js
+++ src/controller/Auth.js
@@ -4,38 +4,53 @@ const authConfig = require('../config/auth')
 
 // Models
 const User = require('../models/User')
+const Stat = require('../models/Stat')
+const Activity = require('../models/Activity')
 const sequelize = require('../database')
 
 module.exports = {
     async login(req, res) {
         const { email, password } = req.body
 
-        const user = await User.findOne({
-            where: { email }
+        const getUser = await Stat.findOne({
+            where: { isCorrect: true },
+            attributes: [
+                [sequelize.fn('SUM', sequelize.col('xp')), 'xp'],
+            ],
+            include: [
+                {
+                    model: Activity,
+                    as: 'activities',
+                    attributes: [],
+                    required: true,
+                }, {
+                    model: User,
+                    as: 'user',
+                    where: { email },
+                    required: true,
+                }
+            ],
+            group: ['user.id']
         })
 
-        if (!user) {
-            return res.status(400).json({ error: 'User not found' })
+        if (!getUser) {
+            return res.status(400).json({ error: 'Invalid email or password' })
         }
 
-        if (!(await bcrypt.compare(password, user.password))) {
-            return res.status(400).json({ error: "Invalid password" })
+        if (!(await bcrypt.compare(password, getUser.user.password))) {
+            return res.status(400).json({ error: "Invalid email or password" })
         }
-
-        const xp = await getXp({
-            where: { email: user.email }
-        })
 
         return await res.json({
-            token: generateToken(user.id),
+            token: generateToken(getUser.user.id),
             user: {
-                id: user.id,
-                name: user.name,
-                lastname: user.lastname,
-                email: user.email,
-                birth: user.birth,
-                gender: user.gender,
-                xp
+                id: getUser.user.id,
+                name: getUser.user.name,
+                lastname: getUser.user.lastname,
+                email: getUser.user.email,
+                birth: getUser.user.birth,
+                gender: getUser.user.gender,
+                xp: getUser.toJSON().xp
             }
         })
     },
@@ -75,21 +90,3 @@ module.exports = {
 const generateToken = (id) => jwt.sign({ id }, authConfig.secret, {
     expiresIn: "15d"
 })
-
-
-const getXp = async ({ where = { email: '' } }) => {
-
-    const { email } = where
-
-    const query = `
-    SELECT CASE WHEN COUNT(a.xp) = 0 THEN 0 ELSE SUM(a.xp) END AS xp
-    FROM Users u
-    INNER JOIN Stats s ON s.Users_id = u.id AND u.email LIKE '${email}'
-    INNER JOIN Activities a ON a.id = s.Activity_id
-    WHERE s.isCorrect = true
-    `
-    // Get XP user
-    const response = await sequelize.query(query, { type: sequelize.QueryTypes.SELECT })
-
-    return response[0].xp
-}
